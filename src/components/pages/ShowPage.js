44c575f7e91625a230ca639de51b4e03e6e54130
ManojParmar7 <manojparmar6161@gmail.com>
2024-01-29 08:05:48 -0500

    update components my show

--- src/components/pages/ShowPage.js
+++ src/components/pages/ShowPage.js
@@ -15,9 +15,16 @@ const ShowPage = () => {
     getAllShows();
   }, []);
 
+  // Sort shows by name in ascending order by default
+  const sortedShows = [...shows].sort((a, b) => {
+    const nameA = a.show ? a.show.name : a.name;
+    const nameB = b.show ? b.show.name : b.name;
+    return nameA.localeCompare(nameB);
+  });
+
   const indexOfLastShow = currentPage * showsPerPage;
   const indexOfFirstShow = indexOfLastShow - showsPerPage;
-  const currentShows = shows.slice(indexOfFirstShow, indexOfLastShow);
+  const currentShows = sortedShows.slice(indexOfFirstShow, indexOfLastShow);
 
   const handlePageChange = (event, value) => {
     setCurrentPage(value);
@@ -25,65 +32,64 @@ const ShowPage = () => {
 
   return (
     <>
-    <Container sx={{ maxWidth: 'lg', paddingTop: 4, paddingBottom: 4 }}>
-      <Searchbar />
-      {loading ? (
-        <Loader />
-      ) : (
-        <Box>
-          <Grid container spacing={3}>
-            {currentShows.map((item) => (
-              item && item.show ? (
-                <ListItem
-                  key={item && item.show.id}
-                  id={item && item.show.id}
-                  image={
-                    item && item.show.image
-                      ? item && item.show.image.medium
-                      : "https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg"
-                  }
-                  name={item && item.show.name}
-                  rating={
-                    item && item.show.rating.average
-                      ? item && item.show.rating.average
-                      : "No rating"
-                  }
-                />
-              ) : (
-                <ListItem
-                  key={item && item.id}
-                  id={item && item.id}
-                  image={
-                    item && item.image
-                      ? item && item.image.medium
-                      : "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg"
-                  }
-                  name={item && item.name}
-                  rating={
-                    item && item.rating.average
-                      ? item && item.rating.average
-                      : "No rating"
-                  }
-                />
-              )
-            ))}
-          </Grid>
-          <Pagination
-    count={Math.ceil(shows.length / showsPerPage)}
-    page={currentPage}
-    onChange={handlePageChange}
-    renderItem={(item) => <PaginationItem component="div" {...item} />}
-    sx={{
-      justifyContent: 'center',
-      marginTop: 2,
-      marginBottom: 4, // Adjust the margin as needed
-    }}
-  />
-        </Box>
-      )}
-    </Container>
-    
-  </>
+      <Container sx={{ maxWidth: 'lg', paddingTop: 4, paddingBottom: 4 }}>
+        <Searchbar />
+        {loading ? (
+          <Loader />
+        ) : (
+          <Box>
+            <Grid container spacing={3}>
+              {currentShows.map((item) => (
+                item && item.show ? (
+                  <ListItem
+                    key={item && item.show.id}
+                    id={item && item.show.id}
+                    image={
+                      item && item.show.image
+                        ? item && item.show.image.medium
+                        : "https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg"
+                    }
+                    name={item && item.show.name}
+                    rating={
+                      item && item.show.rating.average
+                        ? item && item.show.rating.average
+                        : "No rating"
+                    }
+                  />
+                ) : (
+                  <ListItem
+                    key={item && item.id}
+                    id={item && item.id}
+                    image={
+                      item && item.image
+                        ? item && item.image.medium
+                        : "https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg"
+                    }
+                    name={item && item.name}
+                    rating={
+                      item && item.rating.average
+                        ? item && item.rating.average
+                        : "No rating"
+                    }
+                  />
+                )
+              ))}
+            </Grid>
+            <Pagination
+              count={Math.ceil(sortedShows.length / showsPerPage)}
+              page={currentPage}
+              onChange={handlePageChange}
+              renderItem={(item) => <PaginationItem component="div" {...item} />}
+              sx={{
+                justifyContent: 'center',
+                marginTop: 2,
+                marginBottom: 4, // Adjust the margin as needed
+              }}
+            />
+          </Box>
+        )}
+      </Container>
+    </>
   );
 };
 
